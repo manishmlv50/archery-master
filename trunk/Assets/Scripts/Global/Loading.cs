b15cc6db283f54e38ac0ca5af8931aeaca456628
irvingliao@gmail.com <irvingliao@gmail.com@17d9e084-c290-5e5a-d58f-36aba619197f>
2011-10-07 23:30:10 +0000

    Some adjustment of startmenu

--- trunk/Assets/Scripts/Global/Loading.cs
+++ trunk/Assets/Scripts/Global/Loading.cs
@@ -19,9 +19,9 @@ public class Loading : MonoBehaviour {
 		// if loading the other level, then pop up a message of "Loading.....%"
 		if(LOAD) {
 			GUIStyle loadStyle = new GUIStyle(GUI.skin.box);
-			GUI.skin.box.fontSize = 22 * Screen.height/640;
+			GUI.skin.box.fontSize = 32 * Screen.height/640;
 			percentageLoaded = Application.GetStreamProgressForLevel("Level") * 100;
-			GUI.Box(new Rect(0.4f*Screen.width, 0.4f*Screen.height, 180*Screen.width/960, 40*Screen.height/640),
+			GUI.Box(new Rect(0.3f*Screen.width, 0.33f*Screen.height, 0.4f*Screen.width, 0.1f*Screen.height),
 		    	    "Loading..." + percentageLoaded.ToString() + "%", loadStyle);
 		}
 	}
