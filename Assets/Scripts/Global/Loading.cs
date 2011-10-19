0093bc3e356ea5f73c995ff4e413eb95006ebe27
irvingliao <irvingliao@gmail.com>
2011-10-19 23:31:51 +0000

    Adjust the loading fonts

--- Assets/Scripts/Global/Loading.cs
+++ Assets/Scripts/Global/Loading.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Loading : MonoBehaviour {
 	
-	private float percentageLoaded = 0;
+	public GUISkin skin_large;
+	public GUISkin skin_small;
 	
 	public static bool LOAD = false;
 	public static int guiDepth = 0;
+	
+	private float percentageLoaded = 0;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,10 +20,16 @@ public class Loading : MonoBehaviour {
 	
 	void OnGUI() {
 		GUI.depth = guiDepth;
+		GUIStyle loadStyle;
+		if(Screen.width > 900) {
+			loadStyle = new GUIStyle(skin_large.box);
+		}
+		else {
+			loadStyle = new GUIStyle(skin_small.box);
+		}
 		
 		// if loading the other level, then pop up a message of "Loading.....%"
-		if(LOAD) {
-			GUIStyle loadStyle = new GUIStyle(GUI.skin.box);
+		if(LOAD) {	
 			loadStyle.alignment = TextAnchor.MiddleCenter;
 			percentageLoaded = Application.GetStreamProgressForLevel("Level") * 100;
 			GUI.Box(new Rect(0.2f*Screen.width, 0.4f*Screen.height, 0.6f*Screen.width, 0.2f*Screen.height),
