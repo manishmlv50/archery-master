ce5123c0fb84fe2af5a362ab65401e8e9c882b7a
zhangchun88 <zhangchun88@17d9e084-c290-5e5a-d58f-36aba619197f>
2011-11-14 23:01:12 +0000

    halloween clear version

---
+++ branches/halloween/Assets/Scripts/Arrow/Arrow.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+/* Change Log
+ * 
+ * September 24, 2011 - BD
+ *              Added the following targets: BombTarget, FreezeTarget, TimeTarget
+ * */
+
+public class Arrow : MonoBehaviour
+{
+	// Use this for initialization
+	public Transform comboPrefab;
+	private int _combo;
+	
+	public static int DMG_AMOUNT = 25;
+	
+	public int Combo {
+		get {
+			return _combo;
+		}
+		set {
+			_combo = value;
+			if(_combo >=3)
+			{
+				Transform inst = (Transform)Instantiate(comboPrefab,transform.position,Quaternion.identity);
+				Combo com = inst.GetComponent<Combo>();
+				if(com!=null)
+					com.c = Combo;
+			}
+		}
+	}
+	void Start ()
+	{
+		_combo = 0;
+	}
+
+	void OnTriggerEnter (Collider c)
+	{
+		if(c.gameObject.name == "ReflectorWall"){
+			//Debug.print("Entered reflector wall");
+			return;
+		}
+		
+		Target ti = c.gameObject.GetComponent<Target> ();
+		if (ti != null) {
+			ti.DoEffect (this);
+		}
+	}
+}
