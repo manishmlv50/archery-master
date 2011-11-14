ac4b84f2c19c86e26034a768d7666a7782b90732
zhangchun88 <zhangchun88@17d9e084-c290-5e5a-d58f-36aba619197f>
2011-11-14 18:14:52 +0000

    Clear data and add control buttons and use light map to optimize the performance

--- branches/forest/Assets/Scripts/Level/LightControl.cs
+++ branches/forest/Assets/Scripts/Level/LightControl.cs
@@ -1,52 +1,53 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LightControl : MonoBehaviour {
 	
 	private float intensity;
-	public void TurnDark() {
-		Object[] os = GameObject.FindObjectsOfType(typeof(Light));
-		foreach(Object o in os){
-			Light light = (Light) o;
-			if(light.name.Equals("backgroudLight"))
-			{	
-				if(GameStatus.Level == 8)
-					light.enabled = true;
-				else
-					light.enabled = false;
-			}
-			else if(light.name.Equals("light2"))
+	private bool enableLight;
+	public Material black;
+	private static Dictionary<GameObject,Material> materialBackup = new Dictionary<GameObject,Material>();
+	private void switchLight()
+	{
+		GameObject[] go = GameObject.FindGameObjectsWithTag("BackgroundLight");
+		foreach(GameObject light in go){
+			if(GameStatus.Level == 8)
+				light.light.enabled = !enableLight;
+			else
+				light.light.enabled = enableLight;
+		}
+		go = GameObject.FindGameObjectsWithTag("FrontLight");
+		foreach(GameObject light in go){
+			if(GameStatus.Level == 8)
+				light.light.enabled = enableLight;
+			else
+				light.light.enabled = !enableLight;
+		}
+		
+		go = GameObject.FindGameObjectsWithTag("Background");
+		foreach(GameObject gb in go){
+			if(enableLight)
 			{
-				if(GameStatus.Level == 8)
-					light.enabled = false;
-				else
-					light.enabled = true;
+				Material m = null;
+				if(materialBackup.TryGetValue(gb,out m))
+					gb.renderer.material = m;
+			}
+			else{
+				materialBackup[gb] = gb.renderer.material;
+				gb.renderer.material = black;
 			}
 		}
-		Invoke("back",DarknessTarget.DARK_TIME);
+	}
+	public void TurnDark() {
+		enableLight = false;
+		switchLight();
+		enableLight = true;
+		Invoke("switchLight",DarknessTarget.DARK_TIME);
 	}
 	
 	void Start() {
-		back();
-	}
-	
-	void back() {
-		Object[] os = GameObject.FindObjectsOfType(typeof(Light));
-		foreach(Object o in os){
-			Light light = (Light) o;
-			if(light.name.Equals("backgroudLight")) {
-				if(GameStatus.Level == 8)
-					light.enabled = false;
-				else
-					light.enabled = true;
-			}
-			else if(light.name.Equals("light2"))
-			{
-				if(GameStatus.Level == 8)
-					light.enabled = true;
-				else
-					light.enabled = false;
-			}
-		}
+		enableLight = true;
+		switchLight();
 	}
 }
